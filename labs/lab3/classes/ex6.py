47830fed137aa40d1ca2bcec4d07a8f22f49d324
Ilyas <amzehanilias@gmail.com>
2025-02-02 16:02:09 +0500

    finish classes

--- labs/lab3/classes/ex6.py
+++ labs/lab3/classes/ex6.py
@@ -4,7 +4,7 @@ def is_prime(n):
     if n <= 1:
         return False  
     
-    for i in range(2, int(math.sqrt(n)) + 1): 
+    for i in range(2, int(math.sqrt(n)) + 1): #  округляет вниз 
         if n % i == 0:
             return False  
     
